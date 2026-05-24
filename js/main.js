bcc98772ea93c47f24784e0eb46f705187a90af1
La svedese <5554930+sarasjodin@users.noreply.github.com>
2026-05-24 07:12:41 +0200

    verify promos sections and possible acf custom fields

--- js/main.js
+++ js/main.js
@@ -12,8 +12,10 @@ import { setCurrentYear } from './footer.js';
 import { initMenu } from './navigation.js';
 import { fetchCabins } from './cabins.js';
 import { fetchPrices } from './prices.js';
+import { fetchPromos } from './promos.js';
 
 setCurrentYear(); // Adds the current year to the copyright section footer
 initMenu(); // Initializes the navigation menu
 fetchCabins(); // Fetches cabin data from the WordPress REST API
 fetchPrices(); // Fetches cabin price data from the WordPress REST API
+fetchPromos(); // Fetches promo content from the WordPress REST API
